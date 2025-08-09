f723fa796d159697d722ff4359a419da20fd94d0
Thomas <substantialimpulse@pm.me>
2025-08-09 14:23:31 +0200

    initial commit of public repo

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'auto_strom adapter settings': {
+        'en': 'Adapter settings for auto_strom',
+        'de': 'Adaptereinstellungen für auto_strom',
+        'ru': 'Настройки адаптера для auto_strom',
+        'pt': 'Configurações do adaptador para auto_strom',
+        'nl': 'Adapterinstellingen voor auto_strom',
+        'fr': "Paramètres d'adaptateur pour auto_strom",
+        'it': "Impostazioni dell'adattatore per auto_strom",
+        'es': 'Ajustes del adaptador para auto_strom',
+        'pl': 'Ustawienia adaptera dla auto_strom',
+        'zh-cn': 'auto_strom的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
---
+++ src/AutoStromModels.ts
@@ -0,0 +1,48 @@
+export interface ConfigDevice {
+  id: string,
+  priority:number,
+  verbrauch:number,
+  analog:boolean|number|null,
+  analog_min:any,
+  analog_max:any,
+  delay:any,
+  erzeuger:boolean|number|null,
+  l1: boolean|null,
+  l2: boolean|null,
+  l3: boolean|null
+}
+export interface BaseDevice {
+  readonly id: string,
+  readonly priority:number,
+  readonly verbrauch:number,
+  readonly delay_ms:number,
+  readonly analog:boolean,
+  readonly erzeuger:boolean
+  readonly phasen:number[],
+}
+
+export interface AnalogDevice extends BaseDevice {
+  value: number,
+  analog_min:number,
+  analog_max:number
+}
+
+export interface DigitalDevice extends BaseDevice{
+  value: boolean,
+}
+
+export type AutoStromDevice = AnalogDevice | DigitalDevice;
+
+export function isAnalog(param: BaseDevice): param is AnalogDevice {
+  return param.analog === true
+}
+
+export function isDigital(param: BaseDevice): param is DigitalDevice {
+  return !isAnalog(param)
+}
+
+export enum PowerCalcMode {
+  ADDITIONAL_CONSUMABLE,
+  ADDITIONAL_FREEABLE,
+  TOTAL_ANALOG_POWER,
+}
